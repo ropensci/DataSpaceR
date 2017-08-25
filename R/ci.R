5ed6b2410e968653e731f5ee881dbd35e4de5f40
J. Kim <jk@juyeongkim.com>
2017-08-25 16:31:24 -0700

    Add ci.R for CIs

---
+++ R/ci.R
@@ -0,0 +1,5 @@
+netrc <- ifelse(.Platform$OS.type == "windows", "~/_netrc", "~/.netrc")
+write(x = paste("machine dataspace.cavd.org",
+                "login", Sys.getenv("DSR_login"),
+                "password", Sys.getenv("DSR_pwd")),
+      file = netrc)
