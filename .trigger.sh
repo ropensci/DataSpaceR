183d44cd77b33d10d3522f5711b70ae9a3a7be31
J. Kim <jkim2345@fredhutch.org>
2017-11-17 14:31:07 -0800

    Add a trigger to requset a build of DataSpaceR.test

---
+++ .trigger.sh
@@ -0,0 +1,14 @@
+body='{
+  "request": {
+    "branch": "master",
+    "message": "API Request from DataSpaceR Build"
+  }
+}'
+
+curl -s -X POST \
+-H "Content-Type: application/json" \
+-H "Accept: application/json" \
+-H "Travis-API-Version: 3" \
+-H "Authorization: token $TRAVIS_API_TOKEN" \
+-d "$body" \
+https://api.travis-ci.org/repo/FredHutch%2FDataSpaceR.test/requests
