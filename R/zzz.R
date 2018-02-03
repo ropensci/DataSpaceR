21bf4eba5ed3fb48d2cf8027b47aaf7bbaa090d0
Ju Yeong Kim <jkim2345@fredhutch.org>
2018-02-02 20:41:33 -0800

    Add a package startup message on the terms of use and add .onUnload to undo global settings on Windows

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,11 @@
 .onAttach <- function(libname, pkgname) {
-  def_netrc <- ifelse(.Platform$OS.type == "windows", "~/_netrc", "~/.netrc")
+  packageStartupMessage("By exporting data from the CAVD DataSpace, you agree to be bound by the Terms of Use available on the CAVD DataSpace sign-in page at https://dataspace.cavd.org/cds/CAVD/app.view?")
 
-  if (!file.exists(def_netrc) && !exists("labkey.sessionCookieName") && Sys.getenv("DS_login") == "") {
+  netrc <- ifelse(.Platform$OS.type == "windows", "~/_netrc", "~/.netrc")
+
+  if (!file.exists(netrc) &&
+      !exists("labkey.sessionCookieName") &&
+      Sys.getenv("DS_login") == "") {
     packageStartupMessage("A netrc file is required to connect to the DataSpace.")
   }
 }
@@ -9,7 +13,20 @@
 .onLoad <- function(libname, pkgname) {
   # set ca bundle file path for windows
   if (.Platform$OS.type == "windows") {
-    options(RCurlOptions = list(cainfo = system.file("ssl_certs/cacert.pem",
-                                                     package = pkgname)))
+    options(
+      RCurlOptions = list(
+        cainfo = system.file(
+          "ssl_certs/cacert.pem",
+          package = pkgname
+        )
+      )
+    )
+  }
+}
+
+.onUnload <- function(libname, pkgname) {
+  # remove ca bundle file path for windows
+  if (.Platform$OS.type == "windows") {
+    options(RCurlOptions = NULL)
   }
 }
