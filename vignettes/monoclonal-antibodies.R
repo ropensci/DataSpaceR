59d90448c216d26c20937532d6c541512824ae05
J. Kim <jkim2345@fredhutch.org>
2018-08-20 15:12:59 -0700

    Create vignette for mAb

---
+++ vignettes/monoclonal-antibodies.R
@@ -0,0 +1,35 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ------------------------------------------------------------------------
+library(DataSpaceR)
+con <- connectDS()
+
+DT::datatable(con$mabGrid)
+
+## ------------------------------------------------------------------------
+# filter the grid by HXB2 location (Env and 249M Gag)
+con$filterMabGrid(using = "hxb2_location", value = c("Env", "Gag"))
+
+# filter the grid by species (llama)
+con$filterMabGrid(using = "donor_species", value = "llama")
+
+# check the updated grid
+DT::datatable(con$mabGrid)
+
+# retrieve available viruses in the grid
+con$retrieveMabGridValue(using = "viruses")
+
+# retrive available clades for 1H9 mAb mixture in the grid
+con$retrieveMabGridValue(using = "clades", mAb_mixture = "1H9")
+
+## ------------------------------------------------------------------------
+mab <- con$getMab()
+mab
+
+## ----session-info--------------------------------------------------------
+sessionInfo()
+
