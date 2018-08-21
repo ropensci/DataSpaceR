3381d5cdf1906f83c2f48166f8a740dc77755c80
Ju Yeong Kim <jkim2345@fredhutch.org>
2018-08-21 08:23:10 -0700

    Update mAb vignette

--- vignettes/monoclonal-antibodies.R
+++ vignettes/monoclonal-antibodies.R
@@ -8,7 +8,7 @@ knitr::opts_chunk$set(
 library(DataSpaceR)
 con <- connectDS()
 
-DT::datatable(con$mabGrid)
+DT::datatable(con$mabGrid, options=list(autoWidth = TRUE, scrollX = TRUE))
 
 ## ------------------------------------------------------------------------
 # filter the grid by HXB2 location (Env and 249M Gag)
@@ -18,8 +18,13 @@ con$filterMabGrid(using = "hxb2_location", value = c("Env", "Gag"))
 con$filterMabGrid(using = "donor_species", value = "llama")
 
 # check the updated grid
-DT::datatable(con$mabGrid)
+DT::datatable(con$mabGrid, options=list(autoWidth = TRUE, scrollX = TRUE))
 
+## ----eval=FALSE----------------------------------------------------------
+#  # Or
+#  con$filterMabGrid(using = "hxb2_location", value = c("Env", "Gag"))$filterMabGrid(using = "donor_species", value = "llama")$mabGrid
+
+## ------------------------------------------------------------------------
 # retrieve available viruses in the grid
 con$retrieveMabGridValue(using = "viruses")
 
@@ -29,6 +34,9 @@ con$retrieveMabGridValue(using = "clades", mAb_mixture = "1H9")
 ## ------------------------------------------------------------------------
 mab <- con$getMab()
 mab
+
+## ------------------------------------------------------------------------
+DT::datatable(mab$nabMab, options=list(autoWidth = TRUE, scrollX = TRUE))
 
 ## ----session-info--------------------------------------------------------
 sessionInfo()
