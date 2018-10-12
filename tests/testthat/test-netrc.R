796349f9abc6a2737075bfb24439ca0772a422bc
Ju Yeong Kim <jkim2345@fredhutch.org>
2018-10-12 08:10:56 -0700

    Update netrc tests

--- tests/testthat/test-netrc.R
+++ tests/testthat/test-netrc.R
@@ -1,16 +1,26 @@
 context("netrc")
 
 test_that("`writeNetrc`", {
-  netrcFile <- try(writeNetrc("fake@email.org", "fakePwd"), silent = TRUE)
+  temp <- tempfile()
+  netrcFile <- try(
+    writeNetrc("fake@email.org", "fakePwd", netrcFile = temp),
+    silent = TRUE
+  )
 
   expect_is(netrcFile, "character")
+  expect_equal(netrcFile, temp)
   expect_true(file.exists(netrcFile))
-  expect_match(readLines(netrcFile),
-               "machine dataspace.cavd.org login fake@email.org password fakePwd")
+  expect_match(
+    readLines(temp),
+    "machine dataspace.cavd.org login fake@email.org password fakePwd"
+  )
 })
 
 test_that("`checkNetrc`", {
-  netrcFile <- try(checkNetrc(), silent = TRUE)
+  netrcFile <- try(
+    checkNetrc(),
+    silent = TRUE
+  )
 
   expect_is(netrcFile, "character")
   expect_true(file.exists(netrcFile))
