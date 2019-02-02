4d6dbc307b868d0b430b2147d3fece6128621bde
Ju Yeong Kim <jkim2345@fredhutch.org>
2019-02-01 20:40:59 -0800

    Trigger only in linux build

--- .trigger.sh
+++ .trigger.sh
@@ -1,3 +1,5 @@
+if [ "$TRAVIS_OS_NAME" == "linux"]
+then
 body='{
   "request": {
     "branch": "master",
@@ -12,3 +14,5 @@ curl -s -X POST \
   -H "Authorization: token $TRAVIS_API_TOKEN" \
   -d "$body" \
   https://api.travis-ci.org/repo/CAVDDataSpace%2FDataSpaceR.test/requests
+fi
+
