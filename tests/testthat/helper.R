29de8cdcc893905fc700fb6e14174dc5e86451bb
Jason Taylor <you@example.com>
2021-02-19 16:30:24 -0800

    Fix tests for mab object field count and helper test

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,2 +1,2 @@
-assign("onStaging", identical(tolower(Sys.getenv("STAGING")), "true"), .GlobalEnv)
+assign("onStaging", identical(tolower(Sys.getenv("STAGING")), "true"))
 assign("baseUrl", ifelse(onStaging, "https://dataspace-staging.cavd.org", "https://dataspace.cavd.org"))
